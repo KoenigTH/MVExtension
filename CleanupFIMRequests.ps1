3803f7dc3dc45e70c986bf955d8d49a808b78b47
KoenigTH <th@thorstenhennig.de>
2026-07-31 15:45:22 +0200

    Add files via upload

---
+++ CleanupFIMRequests.ps1
@@ -0,0 +1,22 @@
+﻿Add-PSSnapin FIMAutomation
+
+$cutoffDate = (Get-Date).AddDays(-90)
+
+$filter = "/Request[
+    RequestStatus='Completed' and
+    CreatedTime<'$($cutoffDate.ToString("s"))'
+]"
+
+Export-FIMConfig -CustomConfig $filter | `
+ForEach-Object {
+
+    $requestId = $_.ResourceManagementObject.ResourceManagementAttributes[
+        "ObjectID"
+    ].Value
+
+    Write-Host "Deleting Request $requestId"
+
+    Remove-FIMResource `
+        -ObjectID $requestId `
+        -Confirm:$false
+}
---
+++ CountFIMRequests.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module LithnetRMA
+
+$cutoff = (Get-Date).AddDays(-90)
+
+Search-Resources `
+    -ResourceType Request `
+    -AttributesToGet ObjectID, CreatedTime |
+    Where-Object {$_.CreatedTime -lt $cutoff} |
+    Remove-Resource
