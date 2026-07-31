103dd541639cd370c3f1231d265790dcb1de54d5
KoenigTH <th@thorstenhennig.de>
2026-07-31 17:37:18 +0200

    Add files via upload

--- CountFIMRequests.ps1
+++ CountFIMRequests.ps1
@@ -4,3 +4,17 @@ $cutoffDate = (Get-Date).AddDays(-90)
 
 Export-FIMConfig -CustomConfig "/Request[CreatedTime<'$($cutoffDate.ToString("s"))']" |
 Measure-Object
+
+
+--------------
+
+
+
+Add-PSSnapin FIMAutomation
+
+$cutoffDate = (Get-Date).AddDays(-90)
+
+Export-FIMConfig `
+    -OnlyBaseResources `
+    -CustomConfig "/Request[CreatedTime<'$($cutoffDate.ToString("s"))']" |
+    Measure-Object
